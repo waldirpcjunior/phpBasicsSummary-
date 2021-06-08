d24755484545b59659615f23b93ee1efc0ef6389
unknown <waldirpcjunior@gmail.com>
2021-06-08 15:08:52 -0300

    new file:   sumario.php 	deleted:    variaveis_operacoes.php

---
+++ sumario.php
@@ -0,0 +1,28 @@
+<?php  
+
+// declaração de variavel
+$idade = 28;
+$nome = "Waldir";
+
+echo $idade;
+
+//operadores matematicos
+
+$soma = 2 + 2;
+$subitração = 2 - 2;
+$divicao = 2 / 2;
+$mutiplicacao = 2 * 2;
+
+$expoente = 2 ** 2;
+
+$resto = 10 % 3;
+
+// tipo de variaveis
+
+$idade = 28; // integer (float)
+$valorNumeroPi = 3.14; // double (float)
+$nome = "Waldir"; // string 
+$verdadeiro = true ; // boolean
+
+echo gettype($idade); // descobrir o tipo da variavel
+
